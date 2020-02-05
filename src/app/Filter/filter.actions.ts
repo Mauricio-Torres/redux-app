7ea6ac9a0cd5c8913ac27505cbd3fdb355ad2589
Mauricio-Torres <44648203+Mauricio-Torres@users.noreply.github.com>
2020-02-05 03:10:20 -0500

    correccion error

--- src/app/Filter/filter.actions.ts
+++ src/app/Filter/filter.actions.ts
@@ -1,5 +1,4 @@
 import { Action } from '@ngrx/store';
-import { type } from 'os';
 
 export const SET_FILTER = '[Filter] Set Filter';
 export type filtrosValidos = 'todos' | 'completados' | 'pendientes';
